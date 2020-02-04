7963f734c279bd136a478de48016161b62122838
Alex Marchant <alexander.marchant@and.digital>
2020-02-04 14:59:02 +0000

    Initial commit

---
+++ NewsApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NewsApp
+//
+//  Created by Alex Marchant on 04/02/2020.
+//  Copyright © 2020 Alex Marchant. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
