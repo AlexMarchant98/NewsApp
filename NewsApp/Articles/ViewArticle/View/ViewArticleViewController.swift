f521856f9107f7f73b829c46d2b3b1331789b3d8
Alex Marchant <alexander.marchant@and.digital>
2020-02-05 14:10:29 +0000

    Add ViewArticleCell to display selected article

--- NewsApp/Articles/ViewArticle/View/ViewArticleViewController.swift
+++ NewsApp/Articles/ViewArticle/View/ViewArticleViewController.swift
@@ -12,11 +12,37 @@ class ViewArticleViewController: UIViewController, Storyboarded {
     
     var viewArticlePresenter: ViewArticlePresenterProtocol!
     var viewedArticle: Article!
+    
+    @IBOutlet weak var tableView: UITableView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+        tableView.register(UINib(nibName: Constants.ViewArticleCellNibName, bundle: nil), forCellReuseIdentifier: Constants.ViewArticleCellIdentifier)
     }
 
+}
+
+extension ViewArticleViewController: UITableViewDataSource, UITableViewDelegate {
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: Constants.ViewArticleCellIdentifier, for: indexPath) as! ViewArticleCell
+        
+        cell.article = viewedArticle
+        
+        return cell
+    }
+    
 }
 
 extension ViewArticleViewController: ViewArticlePresenterView {
