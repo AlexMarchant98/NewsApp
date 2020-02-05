e201ddf31f3a8c848b7fd591abfc99f417a8d854
Alex Marchant <alexander.marchant@and.digital>
2020-02-05 13:43:42 +0000

    Add ViewArticleCell

---
+++ NewsApp/Articles/ViewArticle/View/ViewArticleCell/ViewArticleCell.swift
@@ -0,0 +1,65 @@
+//
+//  ViewArticleCell.swift
+//  NewsApp
+//
+//  Created by Alex Marchant on 05/02/2020.
+//  Copyright © 2020 Alex Marchant. All rights reserved.
+//
+
+import UIKit
+
+class ViewArticleCell: UITableViewCell {
+    
+    @IBOutlet weak var articleImage: UIImageView!
+    @IBOutlet weak var title: UILabel!
+    @IBOutlet weak var story: UILabel!
+    @IBOutlet weak var publishTime: UILabel!
+    @IBOutlet weak private var activityIndicator: UIActivityIndicatorView!
+    
+    public var article: Article! {
+        didSet {
+            self.updateView()
+        }
+    }
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+    func updateView() {
+        activityIndicator.startAnimating()
+        activityIndicator.isHidden = false
+        
+        ImageTransfererService.shared.downloadImage(urlString: article.urlToImage ?? "") { (imageData) in
+            
+            defer {
+                DispatchQueue.main.async {
+                    self.activityIndicator.stopAnimating()
+                    self.activityIndicator.isHidden = true
+                }
+            }
+            
+            DispatchQueue.main.async {
+                if let data = imageData {
+                    self.articleImage.image = UIImage(data: data)
+                } else {
+                    self.articleImage.image = UIImage(named: "image-not-found")
+                }
+                self.setNeedsDisplay()
+            }
+            
+        }
+        
+        self.title.text = article.title
+        self.story.text = article.description
+        self.publishTime.text = article.publishedAt
+    }
+    
+}
